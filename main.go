1c07ece2c537174bafa373aa022019585380c513
Björn Berg <32747776+MisterMountain@users.noreply.github.com>
2023-05-09 13:02:13 +0200

    Function inputerror (#8)
    * initial move into a inputerror function
    
    * added usage to function

--- main.go
+++ main.go
@@ -24,20 +24,7 @@ func main() {
 
 	config.ParseArguments()
 
-	if *hostname == "" || *file == "" {
-		var missing string
-		if *hostname == "" && *file == "" {
-			missing = "hostname and file"
-		}
-		if *hostname == "" && *file != "" {
-			missing = "hostname"
-		}
-		if *hostname != "" && *file == "" {
-			missing = "file"
-		}
-		config.FlagSet.Usage()
-		check.Exitf(check.Unknown, "Missing required arguments: %s", missing)
-	}
+	inputError(*hostname, *file, config)
 
 	// Parse the TFTP server address from the hostname flag
 	address := fmt.Sprintf("%s:69", *hostname)
@@ -70,6 +57,23 @@ func main() {
 	}
 
 	check.Exitf(check.OK, "%d bytes received", n)
+}
+
+func inputError(hostname string, file string, config *check.Config) {
+	if hostname == "" || file == "" {
+		var missing string
+		if hostname == "" && file == "" {
+			missing = "hostname and file"
+		}
+		if hostname == "" && file != "" {
+			missing = "hostname"
+		}
+		if hostname != "" && file == "" {
+			missing = "file"
+		}
+		config.FlagSet.Usage()
+		check.Exitf(check.Unknown, "Missing required arguments: %s", missing)
+	}
 }
 
 func verifyChecksum(file string, checksum string) bool {
